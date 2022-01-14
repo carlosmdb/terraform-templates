cca8e6cababf5fd29456059010b22b17710b62b3
Carlos Castro <carlos.castro@mongodb.com>
2022-01-14 09:31:14 +0000

    add comments around BYOK;

--- atlas/clusters/atlas-cluster.tf
+++ atlas/clusters/atlas-cluster.tf
@@ -4,7 +4,11 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
   #cluster_type   = "SHARDED"
   cluster_type = "REPLICASET"
   #disk_size_gb                = 256
-  encryption_at_rest_provider = "AWS"
+
+# uncomment the following line if you want to use AWS KMS
+# * make sure you have created it using one of the existing templates *
+
+# encryption_at_rest_provider = "AWS"
   mongo_db_major_version      = "5.0"
   backup_enabled              = true
   pit_enabled                 = true
@@ -20,7 +24,7 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
       region_name   = local.region
       priority      = 7
     }
-
+# uncomment the following line if you want to create multi-cloud clusters
 /*     region_configs {
       electable_specs {
         instance_size = "M10"
@@ -38,6 +42,9 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
   }
 
   # Encryption needs to be turned on for the project before it can be enabled on the cluster
+  # uncomment the following line if you want to use AWS KMS
+  # * make sure you have created it using one of the existing templates *
+
   #depends_on = [mongodbatlas_encryption_at_rest.encrypt]
 }
 
