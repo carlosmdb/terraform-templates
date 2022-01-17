652713fcf18c4690d227830ac473edd34bf82440
Carlos Castro <carlos.castro@mongodb.com>
2022-01-17 10:55:14 +0000

    fix aws private endpoint;

--- atlas/aws-kms/locals.tf
+++ atlas/aws-kms/locals.tf
@@ -16,36 +16,6 @@ locals {
 
   common_name               = lower("${var.mongodb_project_name}-mongodbatlas-encrypt")
   encrypt_at_rest_key_alias = "alias/${local.common_name}"
-
-  user_data = <<-EOT
-  #!/bin/bash
-  
-  cat << EOF >> /etc/security/limits.conf
-  * soft     nproc          65535    
-  * hard     nproc          65535   
-  * soft     nofile         65535   
-  * hard     nofile         65535
-  root soft     nproc          65535    
-  root hard     nproc          65535   
-  root soft     nofile         65535   
-  root hard     nofile         65535
-  EOF
-
-  cat << EOF >> /etc/pam.d/common-session
-  session required pam_limits.so
-  EOF
-
-  apt-get -y update && apt-get -y install python3 python3-pip git python3.8-venv
-  pip3 install pymongo==4.0.1
-  pip3 install faker
-  pip3 install dnspython
-
-  wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
-  echo 'deb [ arch=amd64,arm64 ] http://repo.mongodb.com/apt/ubuntu bionic/mongodb-enterprise/5.0 multiverse' | tee /etc/apt/sources.list.d/mongodb-enterprise.list
-  apt-get update
-  apt-get install -y mongodb-enterprise mongodb-enterprise-shell mongodb-enterprise-tools
-
-  EOT
 }
 
 terraform {
--- atlas/aws-private-endpoint/variables.tf
+++ atlas/aws-private-endpoint/variables.tf
@@ -1,57 +1,27 @@
-variable "cloud_provider_access_name" {
-  default = "AWS"
+variable "atlas_public_key" {
+  description = "MongoDB Atlas Public Key"
+  type = string
 }
-variable "public_key" {
-
+variable "atlas_private_key" {
+  description = "MongoDB Atlas Private Key"
+  type = string
 }
-variable "private_key" {
-
-}
-variable "access_key" {
-
-}
-variable "secret_key" {
-
-}
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "mongodb_project_name" {
-
-}
-variable "enable_kms_key_rotation" {
-  default = "true"
-}
-variable "mongodb_cluster_name" {
-  default = "atlas"
-}
-
-variable "admin_password" {
-  description = "Password for default users"
-  type        = string
-}
-variable "key_name" {
-  description = "Key pair name"
-  type        = string
-}
-
-variable "private_key_path" {
-  description = "Access path to private key"
-  type        = string
-}
-
-variable "provisioning_address_cdr" {
-  description = "SSH firewall source address, home/office !?"
-  type        = string
-}
-
 variable "user_email" {
   description = "Email address to add as tag"
   type        = string
   default     = "somebody@example.com"
 }
 variable "project_id" {
-  description = "Atlas Existin Project ID Key"
+  description = "Atlas Existing Project ID Key"
+  type        = string
+}
+
+variable "mongodb_project_name" {
+  description = "Atlas Existing Project name"
+  type        = string
+}
+
+variable "provisioning_address_cdr" {
+  description = "Your provisioning address cidr block"
   type        = string
 }
